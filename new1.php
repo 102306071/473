ed4f3ebdacfd1749a2c02e3762166b1a29544001
102306071 <102306071@nccu.edu.tw>
2016-11-25 16:48:13 +0800

    edit
    edit

--- new1.php
+++ new1.php
@@ -2,7 +2,7 @@
 <link href="css/new1.css" rel="stylesheet">
 <script type="text/javascript">
 $(document).ready(function() {
-$('#right').css({"height":"8800px"});
+$('#right').css({"height":"9500px"});
 
 });//end ready
 
